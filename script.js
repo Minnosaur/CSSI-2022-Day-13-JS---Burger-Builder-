90762724df8c041e5c5ee79bcb299e9f87c706e5
Kathy  Lo <108971465+Minnosaur@users.noreply.github.com>
2022-07-27 17:26:14 +0000

    Add more ingredients using nav bar
    Can only click checkout once

--- script.js
+++ script.js
@@ -6,15 +6,25 @@ toppings.forEach((topping) => {
   });
 });
 
- const checkoutButton = document.querySelector(".button");
- const ingredients = document.querySelectorAll(".order .topping"); //html line 10 =.order -- get all the .toppings class **inside only .order div** (there are other topping classes in the #nav, etc)
-
+const checkoutButton = document.querySelector(".button");
  const answerHolder = document.querySelector("#total");
- let total = 0;
-
  checkoutButton.addEventListener("click", () => {
+   let total = 0;
+   const ingredients = document.querySelectorAll(".order .topping"); //html line 10 =.order -- get all the .toppings class **inside only .order div** (there are other topping classes in the #nav, etc)
+
    ingredients.forEach((ingredient) => {
      total += Number(ingredient.dataset.price);
    });
    answerHolder.innerHTML = `The total cost of your burger is $${total}`;
  });
+
+const orderDiv = document.querySelector(".order");
+const NavButtons = document.querySelectorAll("#nav .topping");
+NavButtons.forEach((navButton) => {
+  navButton.addEventListener("click", () => {
+   //tomato will appear under the bun (under the first tomato div) - bc in css, the tomato class has an ORDER (order: 2)
+   orderDiv.innerHTML +=
+     `<div class="topping ${navButton.dataset.ingredient}" data-ingredient="${navButton.dataset.ingredient}" data-price="${navButton.dataset.price}">${navButton.dataset.ingredient}</div>`;
+   });
+});
+ 
